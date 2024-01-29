7ccefc447835086d3d76559e207981e3fdee9f77
Luke Ganzevoort <13698030+lukeganzevoort@users.noreply.github.com>
2024-01-29 15:05:42 -0500

    update invoice route cache and redirect on form submission

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -15,18 +17,19 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
-    customer_id: formData.get('customerId'),
+    customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  console.log('rawFormData', formData);
-
   // TODO: Handle SQL errors
   await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
