e62cb785284d0b308358e886f6547678208d194d
Luke Ganzevoort <13698030+lukeganzevoort@users.noreply.github.com>
2024-01-30 10:46:54 -0500

    removed debugging error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -90,8 +90,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
-
   try {
     await sql`
       DELETE FROM invoices
